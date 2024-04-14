879f262d218bbbb1e5747abc15c5680ac8610d73
Sori Kalinichenko <98168851+AvisSelger@users.noreply.github.com>
2024-04-14 20:52:47 +0300

    Update task_6and5_chen7and8.py
    Added the use of boot and write to disk in my function...

--- task_6and5_chen7and8.py
+++ task_6and5_chen7and8.py
@@ -134,42 +134,33 @@ def load_data(filename="addressbook.pkl"):
         return AddressBook()
 
 def main():
-    book = AddressBook()
+    book = load_data()
     print("Welcome to the assistant bot!")
     while True:
         user_input = input("Enter a command: ")
         command, *args = parse_input(user_input)
-
         if command in ["close", "exit"]:
             print("Good bye!")
+            save_data(book)
             break
-
         elif command == "hello":
             print("How can I help you?")
-
         elif command == "add":
             print(add_contact(book, *args))
-
         elif command == "change":
             print(change_phone(book, *args))
-
         elif command == "phone":
             print(show_phone(book, *args[0]))
-
         elif command == "all":
             print(show_all_contacts(book))
-
         elif command == "add-birthday":
             print(add_birthday(book, *args))
-
         elif command == "show-birthday":
             print(show_birthday(book, *args[0]))
-
         elif command == "birthdays":
             print(birthdays_this_week(book))
-
         else:
             print("Invalid command.")
 
-    if __name__ == "__main__":
-        main()
+if __name__ == "__main__":
+    main()
